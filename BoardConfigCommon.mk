ae91e9bc6ead20d44218f0cd2cd71fdb2142e3ab
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-06-02 21:18:19 -0500

    Import common defines for qsd8k devices

---
+++ BoardConfigCommon.mk
@@ -0,0 +1,86 @@
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# This file sets variables that control the way modules are built
+# thorughout the system. It should not be used to conditionally
+# disable makefiles (the proper mechanism to control what gets
+# included in a build is to use PRODUCT_PACKAGES in a product
+# definition file).
+
+TARGET_NO_BOOTLOADER := true
+
+# QSD8250
+TARGET_BOARD_PLATFORM := qsd8k
+TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
+
+# Arm (v7a) w/ neon
+TARGET_ARCH_VARIANT := armv7-a-neon
+TARGET_CPU_ABI := armeabi-v7a
+TARGET_CPU_ABI2 := armeabi
+ARCH_ARM_HAVE_TLS_REGISTER := true
+
+# Scorpion
+TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
+
+# Headers
+TARGET_SPECIFIC_HEADER_PATH := device/htc/qsd8k-common/include
+
+# Wifi
+BOARD_WPA_SUPPLICANT_DRIVER := WEXT
+BOARD_WPA_SUPPLICANT_PRIVATE_LIB := lib_driver_cmd_wext
+WPA_SUPPLICANT_VERSION  := VER_0_8_X
+BOARD_WLAN_DEVICE       := bcm4329
+WIFI_DRIVER_MODULE_PATH := "/system/lib/modules/bcm4329.ko"
+WIFI_DRIVER_FW_PATH_STA := "/system/vendor/firmware/fw_bcm4329.bin"
+WIFI_DRIVER_FW_PATH_AP  := "/system/vendor/firmware/fw_bcm4329_apsta.bin"
+WIFI_DRIVER_MODULE_ARG  := "iface_name=wlan firmware_path=/system/vendor/firmware/fw_bcm4329.bin nvram_path=/proc/calibration"
+WIFI_DRIVER_MODULE_NAME := "bcm4329"
+
+# Audio
+BOARD_USES_GENERIC_AUDIO := false
+COMMON_GLOBAL_CFLAGS += -DLEGACY_AUDIO_COMPAT
+
+# Compass/Accelerometer
+BOARD_VENDOR_USE_AKMD := akm8973
+
+# Bluetooth
+BOARD_HAVE_BLUETOOTH := true
+BOARD_HAVE_BLUETOOTH_BCM := true
+
+# Fm radio
+BOARD_HAVE_FM_RADIO := true
+BOARD_GLOBAL_CFLAGS += -DHAVE_FM_RADIO
+
+# Qcom
+BOARD_USES_QCOM_LIBS := true
+BOARD_USES_QCOM_HARDWARE := true
+BOARD_VENDOR_QCOM_AMSS_VERSION := 3200
+COMMON_GLOBAL_CFLAGS += -DQCOM_HARDWARE -DREFRESH_RATE=60
+
+# Hardware rendering
+BOARD_EGL_CFG := device/htc/qsd8k-common/egl.cfg
+USE_OPENGL_RENDERER := true
+# We only have 2 buffers
+COMMON_GLOBAL_CFLAGS += -DMISSING_GRALLOC_BUFFERS
+# Debuging egl
+#COMMON_GLOBAL_CFLAGS += -DEGL_TRACE
+
+# Webkit
+TARGET_FORCE_CPU_UPLOAD := true
+
+# Kernel directory
+TARGET_KERNEL_SOURCE := kernel/htc/qsd8k
+
+# Common
+TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
---
+++ qsd8k.mk
@@ -0,0 +1,119 @@
+#
+# Copyright (C) 2009 The Android Open Source Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# English locale
+PRODUCT_LOCALES := en
+
+# High Density art
+PRODUCT_AAPT_CONFIG := normal hdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
+
+#
+# Required Packages
+#
+
+# Audio
+PRODUCT_PACKAGES += \
+    libaudioutils \
+    audio.a2dp.default \
+    audio.primary.qsd8k \
+    audio_policy.qsd8k
+
+# Adreno
+PRODUCT_PACKAGES += \
+    copybit.qsd8k \
+    gralloc.qsd8k \
+    hwcomposer.qsd8k \
+    libgenlock \
+    libmemalloc \
+    libtilerenderer \
+    libQcomUI
+
+# Omx
+PRODUCT_PACKAGES += \
+    libOmxCore \
+    libOmxVidEnc \
+    libOmxVdec \
+    libstagefrighthw
+
+# Misc
+PRODUCT_PACKAGES += \
+    com.android.future.usb.accessory
+
+#
+# Hardware Rendering Properties
+#
+
+PRODUCT_PROPERTY_OVERRIDES += \
+    debug.sf.hw=1 \
+    debug.composition.type=mdp \
+    debug.gr.numframebuffers=2
+
+# dirty_regions: "false" to disable partial invalidates, override if enabletr=true
+PRODUCT_PROPERTY_OVERRIDES += \
+    hwui.render_dirty_regions=false \
+    hwui.disable_vsync=true \
+    hwui.print_config=choice \
+    debug.enabletr=false
+
+#
+# Dalvik Properties
+#
+
+# dexop-flags: "v=" n|r|a, "o=" n|v|a|f, "m=y" register map
+# v=verify o=optimize: n=none r=remote a=all f=full v=verified
+PRODUCT_PROPERTY_OVERRIDES += \
+    dalvik.vm.lockprof.threshold=500 \
+    dalvik.vm.dexopt-flags=m=y \
+    dalvik.vm.checkjni=false
+
+# Default heap settings for 512mb device
+include frameworks/base/build/phone-hdpi-512-dalvik-heap.mk
+
+# We have enough storage space to hold precise GC data
+PRODUCT_TAGS += dalvik.gc.type-precise
+
+#
+# Wifi
+#
+
+# Properties
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0 \
+    wifi.supplicant_scan_interval=45
+
+#
+# Permissions
+#
+
+PRODUCT_COPY_FILES += \
+    frameworks/base/data/etc/handheld_core_hardware.xml:system/etc/permissions/handheld_core_hardware.xml \
+    frameworks/base/data/etc/android.hardware.camera.flash-autofocus.xml:system/etc/permissions/android.hardware.camera.flash-autofocus.xml \
+    frameworks/base/data/etc/android.hardware.location.gps.xml:system/etc/permissions/android.hardware.location.gps.xml \
+    frameworks/base/data/etc/android.hardware.wifi.xml:system/etc/permissions/android.hardware.wifi.xml \
+    frameworks/base/data/etc/android.hardware.sensor.accelerometer.xml:system/etc/permissions/android.hardware.sensor.accelerometer.xml \
+    frameworks/base/data/etc/android.hardware.sensor.compass.xml:system/etc/permissions/android.hardware.sensor.compass.xml \
+    frameworks/base/data/etc/android.hardware.sensor.proximity.xml:system/etc/permissions/android.hardware.sensor.proximity.xml \
+    frameworks/base/data/etc/android.hardware.sensor.light.xml:system/etc/permissions/android.hardware.sensor.light.xml \
+    frameworks/base/data/etc/android.hardware.touchscreen.multitouch.distinct.xml:system/etc/permissions/android.hardware.touchscreen.multitouch.distict.xml \
+    frameworks/base/data/etc/android.hardware.usb.accessory.xml:system/etc/permissions/android.hardware.usb.accessory.xml \
+    frameworks/base/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+#
+# Stuff common to all HTC phones
+#
+
+$(call inherit-product, device/htc/common/common.mk)
