90989a3cdb6f272cb9c9aa5d5fe158759ff57369
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-07-23 21:28:48 -0500

    qsd8k: adjust cpu settings
    new options now available for ondemand
    tweak ksm settings a bit for lower cpu usage
    
    Change-Id: I42144a56a20b8569ed4b94439e7beec4cbbdfdc6

--- init.qcom.post_boot.sh
+++ init.qcom.post_boot.sh
@@ -6,25 +6,27 @@
 #
 target=`getprop ro.board.platform`
 case "$target" in
+    # QSD8x50: Passion, Bravo, Supersonic, Inc
     "qsd8k")
-        # Lower maximum frequency when screen is off
-        echo 700000 > /sys/devices/system/cpu/cpu0/cpufreq/screen_off_max_freq
-        # Cpu frequencies
-        echo 245000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
-        echo 998400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
         # Cpu Governor
         echo "ondemand" > /sys/devices/system/cpu/cpu0/cpufreq/scaling_governor
-        echo 40000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
         # CpuGovernorService
         chown system /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
         chown system /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+        # Tune for scorpion
+        echo 245000 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_min_freq
+        echo 998400 > /sys/devices/system/cpu/cpu0/cpufreq/scaling_max_freq
+        echo 50000 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_rate
+        echo 90 > /sys/devices/system/cpu/cpufreq/ondemand/up_threshold
+        echo 1 > /sys/devices/system/cpu/cpufreq/ondemand/io_is_busy
+        echo 4 > /sys/devices/system/cpu/cpufreq/ondemand/sampling_down_factor
         # /system/lib/libqct-opt JNI native will write
         # mfreq to set CPU freq to max for performance
         chown root.system /sys/devices/system/cpu/mfreq
         chmod 220 /sys/devices/system/cpu/mfreq
-        # Ksm
-        echo 3000 > /sys/kernel/mm/ksm/sleep_millisecs
-        echo 256 > /sys/kernel/mm/ksm/pages_to_scan
+        # Configure and enable KSM
+        echo 1000 > /sys/kernel/mm/ksm/sleep_millisecs
+        echo 100 > /sys/kernel/mm/ksm/pages_to_scan
         echo 1 > /sys/kernel/mm/ksm/run
     ;;
 esac
