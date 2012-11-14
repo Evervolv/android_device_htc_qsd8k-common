ce97b6896dce6a6dd6279b353a45d1c9cacb2451
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-11-14 04:13:10 -0600

    qsd8k: specify arch
    Change-Id: I74b6c91b87b46f23be097e110d9ecb46869f1aa9

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -25,6 +25,7 @@ TARGET_BOARD_PLATFORM := qsd8k
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno200
 
 # Arm (v7a) w/ neon
+TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
