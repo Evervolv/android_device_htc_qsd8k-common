38f1e90e0bfb33c090171d9855b5272f21c36705
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2012-12-11 13:07:23 -0600

    qsd8k: drop in camera source
    Change-Id: I1beed350a52ba30bd0cc0a9ad614430b3610feba

---
+++ camera/Android.mk
@@ -0,0 +1,25 @@
+$(shell mkdir -p $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/)
+$(shell touch $(OUT)/obj/SHARED_LIBRARIES/libcamera_intermediates/export_includes)
+
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+
+LOCAL_C_FLAGS          += -O3
+LOCAL_MODULE_TAGS      := optional
+LOCAL_MODULE_PATH      := $(TARGET_OUT_SHARED_LIBRARIES)/hw
+LOCAL_MODULE           := camera.$(TARGET_BOARD_PLATFORM)
+
+LOCAL_SRC_FILES        := QcomCamera.cpp
+
+LOCAL_SHARED_LIBRARIES := liblog libdl libutils libcamera_client libbinder \
+                          libcutils libhardware libui libcamera
+
+LOCAL_C_INCLUDES       := frameworks/base/services \
+                          frameworks/base/include \
+                          hardware/libhardware/include \
+                          hardware/qcom/display-legacy/libgralloc
+
+LOCAL_PRELINK_MODULE   := false
+
+include $(BUILD_SHARED_LIBRARY)
