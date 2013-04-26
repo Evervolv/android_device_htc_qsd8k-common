690424404796260eb70f84ad74713f70c17f600d
andrew.boren <andrew.boren@gmail.com>
2013-04-25 20:46:11 -0700

    qsd8k: wifi: support for bcmdhd as a module.
    Change-Id: Ifdc2cc0fd1fc460bb9929a869aec8755f394b59b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -51,6 +51,8 @@ WIFI_DRIVER_FW_PATH_STA          := "/vendor/firmware/fw_bcmdhd.bin"
 WIFI_DRIVER_FW_PATH_AP           := "/vendor/firmware/fw_bcmdhd_apsta.bin"
 WIFI_DRIVER_FW_PATH_P2P          := "/vendor/firmware/fw_bcmdhd_p2p.bin"
 WIFI_DRIVER_FW_PATH_PARAM        := "/sys/module/bcmdhd/parameters/firmware_path"
+WIFI_DRIVER_MODULE_NAME          := bcmdhd
+WIFI_DRIVER_MODULE_PATH          := "/system/lib/modules/bcmdhd.ko"
 BOARD_LEGACY_NL80211_STA_EVENTS := true
 
 # Audio
