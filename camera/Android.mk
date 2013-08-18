5587669e32a38586fc1a899d4cb4104ed6dca3d9
Andrew Sutherland <dr3wsuth3rland@gmail.com>
2013-08-18 17:56:54 -0500

    qsd8k: build camera
    Change-Id: I24722e4af39819410e28ca4f40e3d3c58f4bf6fd

--- camera/Android.mk
+++ camera/Android.mk
@@ -20,6 +20,9 @@ LOCAL_C_INCLUDES       := frameworks/base/services \
                           hardware/libhardware/include \
                           hardware/qcom/display-legacy/libgralloc
 
+#Hack to link libcamera
+LOCAL_LDFLAGS          := -Wl,--no-fatal-warnings
+
 LOCAL_C_INCLUDES              += $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr/include
 LOCAL_ADDITIONAL_DEPENDENCIES := $(TARGET_OUT_INTERMEDIATES)/KERNEL_OBJ/usr
 
