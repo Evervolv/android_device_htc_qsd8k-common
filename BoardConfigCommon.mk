c6b14fdef08c20a637c1648c9ab3ed7353ef2a6e
David Gasaway <ponsasinorem@gmail.com>
2013-08-27 10:57:24 -1000

    BoardConfig: change CPU_VARIANT to scorpion
    Change-Id: I75b7e86bed549a1ab47b1841b433264b9ce6100b

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -31,7 +31,7 @@ TARGET_ARCH := arm
 TARGET_ARCH_VARIANT := armv7-a-neon
 TARGET_CPU_ABI := armeabi-v7a
 TARGET_CPU_ABI2 := armeabi
-TARGET_CPU_VARIANT := cortex-a8
+TARGET_CPU_VARIANT := scorpion
 
 # Scorpion
 TARGET_USE_SCORPION_BIONIC_OPTIMIZATION := true
